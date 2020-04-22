351df85323c574d734c4cbb9ed2a694a536f9db1
TheNicktator <elsrjefe@gmail.com>
2020-04-21 19:36:09 -0500

    Update HW 10.py

--- HW 10.py
+++ HW 10.py
@@ -4,3 +4,7 @@ filename = str(input('Please enter the file name: ')) #asks user to input file's
 file_data = open(filename, a) #opens declared file in edit mode, new data will be appended to file, sets reference variable
 file_data = [] #set data in file into an array
 file_data.append(print((statistics.mean(file_data))) #will write the average of data at the end of the file, file_data.write() as another possible route?
+
+#def avg(file_data):
+ #   statistics.mean(file_data)
+  #  return
